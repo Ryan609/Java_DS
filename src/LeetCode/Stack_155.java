747c4e56f7730e3acd3a96d69ae3343967b78e10
Xinyan <8380@88.com>
2022-05-10 15:32:01 +0930

    leetcode num155

---
+++ src/LeetCode/Stack_155.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class Stack_155 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int val) {
+        stack1.push(val);
+        if (stack2.isEmpty()) {
+            stack2.push(val);
+        } else {
+            int temp = stack2.peek();
+            stack2.push(Math.min(val, temp));
+        }
+    }
+
+    public void pop() {
+        stack1.pop();
+        stack2.pop();
+    }
+
+    public int top() {
+        return stack1.peek();
+    }
+
+    public int getMin() {
+        return stack2.peek();
+    }
+}
