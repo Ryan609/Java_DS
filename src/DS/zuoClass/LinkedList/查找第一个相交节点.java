1a82dbbe3d83f508b2a1d2b914f5f35456faf08b
Xinyan <8380@88.com>
2022-10-17 17:22:15 +1030

    链表难题之一

---
+++ src/DS/zuoClass/LinkedList/查找第一个相交节点.java
@@ -0,0 +1,110 @@
+package DS.zuoClass.LinkedList;
+
+/**
+ * @author: xinyan
+ * @data: 2022/10/17/15:36
+ **/
+public class 查找第一个相交节点 {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    public static Node getLoopNode(Node head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return null;
+        }
+        Node slow = head;
+        Node fast = head.next.next;
+
+        while (slow != fast) {
+            if (fast.next == null || fast.next.next == null) {
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+
+    // 如果两个链表都无环，返回第一个相交节点，如果不想交，返回null
+    public static Node noLoop(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while (cur1.next != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+        while (cur2.next != null) {
+            n--;
+            cur2 = cur2.next;
+        }
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
+        n = Math.abs(n);
+        while (n != 0) {
+            n--;
+            cur1 = cur1.next;
+        }
+        while (cur1 != cur2) {
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    // 两个有环链表，返回第一个相交节点，如果不想交返回null
+    public static Node bothLoop(Node head1, Node loop1, Node head2, Node loop2) {
+        Node cur1 = null;
+        Node cur2 = null;
+        if (loop1 == loop2) {
+            cur1 = head1;
+            cur2 = head2;
+            int n = 0;
+            while (cur1 != loop1) {
+                n++;
+                cur1 = cur1.next;
+            }
+            while (cur2 != loop2) {
+                n--;
+                cur2 = cur2.next;
+            }
+            cur1 = n > 0 ? head1 : head2;
+            cur2 = cur1 == head1 ? head2 : head1;
+            n = Math.abs(n);
+            while (n != 0) {
+                n--;
+                cur1 = cur1.next;
+            }
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1.next;
+            while (cur1 != loop1) {
+                if (cur1 == loop2) {
+                    return loop1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
+    }
+}
