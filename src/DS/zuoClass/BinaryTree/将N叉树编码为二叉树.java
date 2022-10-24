5b19b6a8b45f60a5b0de3fb5a0c8c90cc8a9b209
Xinyan <8380@88.com>
2022-10-24 11:00:00 +1030

    update

--- src/DS/zuoClass/BinaryTree/将N叉树编码为二叉树.java
+++ src/DS/zuoClass/BinaryTree/将N叉树编码为二叉树.java
@@ -34,31 +34,48 @@ public class 将N叉树编码为二叉树 {
 
     class Codec {
 		// Encodes an n-ary tree to a binary tree.
-		// public TreeNode encode(Node root) {
-		// 	if (root == null) {
-        //         return null;
-        //     }
-        //     TreeNode head = new TreeNode(root.val);
-        //     head.left = en(root.children);
-        //     return head;
-		// }
+		 public TreeNode encode(Node root) {
+		 	if (root == null) {
+                 return null;
+             }
+             TreeNode head = new TreeNode(root.val);
+             head.left = en(root.children);
+             return head;
+		 }
 
-		// private TreeNode en(List<Node> children) {
-		// 	TreeNode head = null;
-        //     TreeNode cur = null;
-        //     for (Node child : children) {
-        //         TreeNode tNode = new TreeNode(child.val);
-        //     }
-		// }
+		 private TreeNode en(List<Node> children) {
+		 	TreeNode head = null;
+             TreeNode cur = null;
+             for (Node child : children) {
+                 TreeNode tNode = new TreeNode(child.val);
+				 if (head == null) {
+					 head = tNode;
+				 } else {
+					 cur.right = tNode;
+				 }
+				 cur = tNode;
+				 cur.left = en(child.children);
+             }
+			 return head;
+		 }
 
-		// // Decodes your binary tree to an n-ary tree.
-		// public Node decode(TreeNode root) {
-		// 	return null;
-		// }
+		 // Decodes your binary tree to an n-ary tree.
+		 public Node decode(TreeNode root) {
+			 if (root == null) {
+				 return null;
+			 }
+			 return new Node(root.val, de(root.left));
+		 }
 
-		// public List<Node> de(TreeNode root) {
-		// 	return null;
-		// }
+		 public List<Node> de(TreeNode root) {
+			List<Node> children = new ArrayList<>();
+			 while (root != null) {
+				 Node cur = new Node(root.val, de(root.left));
+				 children.add(cur);
+				 root = root.right;
+			 }
+			 return children;
+		 }
 
 	}
 
