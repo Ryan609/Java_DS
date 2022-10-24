b2e5c11837d9bb599a4f40db64496ecb41012c79
Xinyan <8380@88.com>
2022-10-24 15:11:47 +1030

    N叉树转二叉树

--- src/DS/zuoClass/BinaryTree/将N叉树编码为二叉树.java
+++ src/DS/zuoClass/BinaryTree/将N叉树编码为二叉树.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class 将N叉树编码为二叉树 {
-    
-    public static class Node {
+
+	public static class Node {
 		public int val;
 		public List<Node> children;
 
@@ -20,7 +20,10 @@ public class 将N叉树编码为二叉树 {
 			val = _val;
 			children = _children;
 		}
-	};
+	}
+
+	;
+
 	// 提交时不要提交这个类
 	public static class TreeNode {
 		int val;
@@ -32,51 +35,49 @@ public class 将N叉树编码为二叉树 {
 		}
 	}
 
-    class Codec {
+	class Codec {
 		// Encodes an n-ary tree to a binary tree.
-		 public TreeNode encode(Node root) {
-		 	if (root == null) {
-                 return null;
-             }
-             TreeNode head = new TreeNode(root.val);
-             head.left = en(root.children);
-             return head;
-		 }
+		public TreeNode encode(Node root) {
+			if (root == null) {
+				return null;
+			}
+			TreeNode head = new TreeNode(root.val);
+			head.left = en(root.children);
+			return head;
+		}
 
-		 private TreeNode en(List<Node> children) {
-		 	TreeNode head = null;
-             TreeNode cur = null;
-             for (Node child : children) {
-                 TreeNode tNode = new TreeNode(child.val);
-				 if (head == null) {
-					 head = tNode;
-				 } else {
-					 cur.right = tNode;
-				 }
-				 cur = tNode;
-				 cur.left = en(child.children);
-             }
-			 return head;
-		 }
+		private TreeNode en(List<Node> children) {
+			TreeNode head = null;
+			TreeNode cur = null;
+			for (Node child : children) {
+				TreeNode tNode = new TreeNode(child.val);
+				if (head == null) {
+					head = tNode;
+				} else {
+					cur.right = tNode;
+				}
+				cur = tNode;
+				cur.left = en(child.children);
+			}
+			return head;
+		}
 
-		 // Decodes your binary tree to an n-ary tree.
-		 public Node decode(TreeNode root) {
-			 if (root == null) {
-				 return null;
-			 }
-			 return new Node(root.val, de(root.left));
-		 }
+		// Decodes your binary tree to an n-ary tree.
+		public Node decode(TreeNode root) {
+			if (root == null) {
+				return null;
+			}
+			return new Node(root.val, de(root.left));
+		}
 
-		 public List<Node> de(TreeNode root) {
+		public List<Node> de(TreeNode root) {
 			List<Node> children = new ArrayList<>();
-			 while (root != null) {
-				 Node cur = new Node(root.val, de(root.left));
-				 children.add(cur);
-				 root = root.right;
-			 }
-			 return children;
-		 }
-
+			while (root != null) {
+				Node cur = new Node(root.val, de(root.left));
+				children.add(cur);
+				root = root.right;
+			}
+			return children;
+		}
 	}
-
 }
